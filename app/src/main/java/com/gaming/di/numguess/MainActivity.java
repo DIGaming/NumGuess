022a47668f643adcc61b353d5ed03f30850b9927
hacksys1337 <hacksys1337@gmail.com>
2017-01-05 15:35:26 -0500

    Second Commit
    Added Point Scoring and Removed Some Associations With C++ Native Code

--- app/src/main/java/com/gaming/di/numguess/MainActivity.java
+++ app/src/main/java/com/gaming/di/numguess/MainActivity.java
@@ -1,8 +1,17 @@
 package com.gaming.di.numguess;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+import static android.R.id.text1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +24,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+    public int RandomNum()
+    {
+        Random newran = new Random();
+        int rannum = newran.nextInt(10 - 1) + 1;
+        return rannum;
+    }
+
+    public void ButtonGuess(View view)
+    {
+        TextView tv1 = (TextView) findViewById(R.id.textView);
+        TextView pntVal = (TextView) findViewById(R.id.points);
+        EditText guess = (EditText) findViewById(R.id.editText2);
+        String cache = String.valueOf(RandomNum());
+
+        if (String.valueOf(guess.getText()).equals(cache))
+        {
+            tv1.setText("Your Value Of " + guess.getText().toString() + " Is Correct");
+            tv1.setTextColor(Color.GREEN);
+            int num = Integer.valueOf(pntVal.getText().toString());
+            int num2 = num + 100;
+            pntVal.setText(String.valueOf(num2));
+        }
+        else
+        {
+            tv1.setText("Your Guess Is Incorrect The Value Is " + cache.toString());
+            tv1.setTextColor(Color.RED);
+        }
     }
 
     /**
